473141288dfa8afe30ade172405633d3ebac7220
two-keys <dandys10@live.com>
2024-09-29 17:31:14 -0500

    Mov everything under src directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Tony's UIUC Training App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
