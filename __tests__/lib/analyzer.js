3ee5df24835f32679d95b0693f21b292b99a9ab1
two-keys <dandys10@live.com>
2024-09-29 22:05:34 -0500

    Group tests for readability

--- __tests__/lib/analyzer.js
+++ __tests__/lib/analyzer.js
@@ -10,32 +10,36 @@ beforeAll(() => {
     testJson = analyzer.loadJson(testDataPath);
 });
 
-test('Can read training json file', () => {
-    // should be Jaelyn
-    let firstUser = trainingJson[0];
+describe('File loading', () => {
+    test('Can read training json file', () => {
+        // should be Jaelyn
+        let firstUser = trainingJson[0];
 
-    expect(firstUser.name).toBe('Jaelyn Quinn');
-});
+        expect(firstUser.name).toBe('Jaelyn Quinn');
+    });
 
-test('Can read test data json file', () => {
-    // should be Antony Sanchez
-    let firstUser = testJson[0];
+    test('Can read test data json file', () => {
+        // should be Antony Sanchez
+        let firstUser = testJson[0];
 
-    expect(firstUser.name).toBe('Antony Sanchez');
-});
+        expect(firstUser.name).toBe('Antony Sanchez');
+    });
+})
 
-// jest doesnt easily allow custom error messages, so we're wrapping everything in an each
-let answerSheet = [
-    ["Coding in Javascript", 2],
-    ["Oracle Database Fundamentals", 1],
-    ["Using Hazardous Chemicals in an Animal Care Facility", 1],
-    ["Coding in C#", 1]
-]
-test.each(answerSheet)('%s should have %i completions', (name, count) => {
-    let completionJson = analyzer.getCompletionCounts(testJson);
+describe('Completion count', () => {
+    // jest doesnt easily allow custom error messages, so we're wrapping everything in an each
+    let answerSheet = [
+        ["Coding in Javascript", 2],
+        ["Oracle Database Fundamentals", 1],
+        ["Using Hazardous Chemicals in an Animal Care Facility", 1],
+        ["Coding in C#", 1]
+    ]
+    test.each(answerSheet)('%s should have %i completions', (name, count) => {
+        let completionJson = analyzer.getCompletionCounts(testJson);
 
-    completionJson.forEach((cBlob) => {
-        if(name == cBlob.name)
-            expect(cBlob.completions).toBe(count)
+        completionJson.forEach((cBlob) => {
+            if(name == cBlob.name)
+                expect(cBlob.completions).toBe(count)
+        });
     });
 });
